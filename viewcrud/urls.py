20d653898dbb45487cae5aa6c77d824f95e8f171
saurav tanwar <tsaurav1711@likelion.org>
2019-07-14 18:42:58 +0900

    thirdcommit

---
+++ viewcrud/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.read, name='home'),
+    path('newblog/',views.create,name="newblog"),
+    path('update/<int:pk>',views.update,name='update'),
+    path('delete/<int:pk>',views.delete,name="delete"),
+]
---
+++ viewcrud/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render,redirect,get_object_or_404
+from django.utils import timezone
+from .models import Blog
+from .forms import NewBlog
+
+def welcome(request):
+    return render(request, 'index.html')
+
+def read(request):
+     blogs=Blog.objects.all()
+     return render(request, 'funccrud.html',{'blogs':blogs})
+
+
+def create(request):
+    if request.method =='POST':
+        form = NewBlog(request.POST)
+        if form.is_valid:
+            post = form.save(commit=False)
+            post.pub_date = timezone.now()
+            post.save()
+            return redirect('home')
+    else:
+        form = NewBlog()
+        return render(request, 'new.html', {'form':form})
+
+def update(request,pk):
+    blog=get_object_or_404(Blog,pk=pk)
+    form=NewBlog(request.POST,instance=blog)
+    if form.is_valid():
+        form.save()
+        return redirect('home')
+    return render(request, 'new.html', {'form':form})
+def delete(request,pk):
+    blog = get_object_or_404(Blog, pk=pk)
+    blog.delete()
+    return redirect('home')
